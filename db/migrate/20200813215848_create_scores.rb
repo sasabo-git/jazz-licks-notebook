235c48d4d1ce0bb5b393679a0e74ab550aebf78c
Kazuki Sasahara <sasabo.pri@gmail.com>
2020-08-14 07:16:36 +0900

    scoreをscaffoldで作成

---
+++ db/migrate/20200813215848_create_scores.rb
@@ -0,0 +1,10 @@
+class CreateScores < ActiveRecord::Migration[6.0]
+  def change
+    create_table :scores do |t|
+      t.text :title
+      t.text :content
+
+      t.timestamps
+    end
+  end
+end
