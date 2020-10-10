4211dd060e6b28822e3ea35b1519313964475515
Kazuki Sasahara <sasabo.pri@gmail.com>
2020-10-10 15:02:38 +0900

    ファイル名の誤記を修正
    herokuにデプロイしたらzeitwerkでエラーが発生したので対応

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,8 +2,8 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
   protected
-    def after_sign_up_path_for(resource)
-      scores_path
+    def after_sign_in_path_for(resource)
+      user_path(resource)
     end
 
     def after_update_path_for(resource)
