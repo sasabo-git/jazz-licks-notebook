9a30909eccc210346f350db91a78748ada587c67
Kazuki Sasahara <sasabo.pri@gmail.com>
2020-10-10 17:11:41 +0900

    アカウント登録できないバグを修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,8 +2,8 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
   protected
-    def after_sign_in_path_for(resource)
-      user_path(resource)
+    def after_sign_up_path_for(resource)
+      scores_path
     end
 
     def after_update_path_for(resource)
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { registrations: "users/registrations",
                                     sessions: "users/sessions" }
+
   resources :users, only: [:show]
   resources :scores
 
